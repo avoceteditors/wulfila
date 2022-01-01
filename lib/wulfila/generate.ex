13e0fc6fcb97b955e8e7ddb3bda8927989d2c67e
Kenneth P. J. Dyer <kenneth@avoceteditors.com>
2022-01-01 17:00:17 -0500

    Completes word generator

--- lib/wulfila/generate.ex
+++ lib/wulfila/generate.ex
@@ -9,11 +9,11 @@ defmodule Wulfila.Generate do
     ])
 
     phonemes = find_phonemes(data)
-
     syllabary = find_syllabary(data, phonemes)
-    IO.inspect syllabary
 
-    #generate(phonemes, syllabary, spec)
+    generate(phonemes, syllabary, spec)
+    |> Enum.join("\n")
+    |> IO.puts
 
   end
 
@@ -37,19 +37,45 @@ defmodule Wulfila.Generate do
   end
 
   def generate(phonemes, syllabary, [count, min, max], ret) do
-    Enum.to_list(0..count)
+
+    count_int =
+      if is_integer(count) do
+        count
+      else
+        String.to_integer(count)
+      end
+
+    min_int =
+      if is_integer(min) do
+        min
+      else
+        String.to_integer(min)
+      end
+
+    max_int =
+      if is_integer(max) do
+        max
+      else
+        String.to_integer(max)
+      end
+
+    words = Enum.to_list(0..count_int)
     |> Enum.map(
       fn _ ->
-        syls = Enum.to_list(min..max) |> Enum.random
+        syls = Enum.to_list(min_int..max_int) |> Enum.random
 
         Enum.to_list(1..syls)
         |> Enum.map(
           fn _ ->
             Enum.random(syllabary)
+            |> Enum.map(fn x -> Enum.random(x) end)
+            |> Enum.join("")
           end
         )
+        |> Enum.join(".")
       end
     )
+    |> Enum.sort
   end
 
   def find_syllabary(data, phonemes) do
@@ -64,12 +90,13 @@ defmodule Wulfila.Generate do
           fn x ->
             key = String.to_atom(x)
 
-            Enum.filter(
-              phonemes,
+            phonemes
+            |> Enum.filter(
               fn ph ->
                 Enum.member?(ph.type, key)
               end
             )
+            |> Enum.map(fn ph -> ph.letter end)
           end)
 
         Enum.map(
